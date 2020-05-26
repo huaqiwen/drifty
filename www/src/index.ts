a8a957d28e3362470d4b0c33e905c6896c4decd9
Qiwen Hua <hqw@huaqiwen.com>
2020-05-26 14:59:15 -0700

    basic implementation of the drifting motion

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,19 +1,25 @@
 import * as BABYLON from "babylonjs";
+import {Engine, Scene, Vector3} from "babylonjs";
 import * as GUI from "babylonjs-gui";
-import { Engine, Scene, Vector3 } from "babylonjs";
-// import * as BBMaterials from "babylonjs-materials";
-
-
 import * as Builder from "./builder";
-import { Direction } from "./types";
-import { Road } from "./models/road";
-import { Game } from "./settings";
+import {Direction} from "./types";
+import {Road} from "./models/road";
+import {Game} from "./settings";
+// import * as BBMaterials from "babylonjs-materials";
 
 
 const canvas = document.getElementById("main_canvas") as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 
 let actionState = Direction.Still;
+
+let movement = {
+    forward: 0,
+    rightward: 0,
+    downward: 0,
+    rotationDelta: 0,
+    state: Direction.Still
+}
 
 async function createScene () {
     let inGameCars = ["viper", "aventador", "shelby1967"];
@@ -51,7 +57,9 @@ async function createScene () {
         panel.removeControl(panel.children[0]);
         const cam = scene.getCameraByName("followCam") as BABYLON.FollowCamera;
         cam.radius = 50; cam.heightOffset = 20; cam.rotationOffset = 180;
-        actionState = Direction.Forward;
+
+        movement.forward = 1;
+        movement.state = Direction.Forward;
     });
 
     return scene;
@@ -64,6 +72,13 @@ createScene().then((result) => {
     engine.runRenderLoop(function () {
         const aventador_root = scene.getNodeByName("aventador");
 
+        aventador_root.position.z += 1.5 * movement.forward;
+        aventador_root.position.x += 1.5 * movement.rightward;
+        aventador_root.rotation = new Vector3(0, Math.PI / 2 + movement.rotationDelta, 0);
+
+        // console.log(movement.forward, movement.rightward);
+
+        /*
         if (actionState == Direction.Forward) {
             scene.getCameraByName("followCam").rotationOffset = 180;
             aventador_root.rotation = new Vector3(0, Math.PI / 2, 0);
@@ -74,12 +89,17 @@ createScene().then((result) => {
             aventador_root.position.x += 1.5;
         }
 
+         */
+
+        /*
         scene.actionManager = new BABYLON.ActionManager(scene);
         // when SPACE key pressed, go right, actionState = -1
         scene.actionManager.registerAction(
             new BABYLON.ExecuteCodeAction(
                 { trigger: BABYLON.ActionManager.OnKeyDownTrigger, parameter: " " },
-                () => { actionState = Direction.Right; }
+                () => {
+                    actionState = Direction.Right;
+                }
             )
         );
         // when SPACE key released, go left, actionState = 1
@@ -90,6 +110,8 @@ createScene().then((result) => {
             )
         );
 
+         */
+
         scene.render();
     });
 });
@@ -98,7 +120,35 @@ createScene().then((result) => {
 window.addEventListener('resize', resize, false);
 window.addEventListener('load', resize, false);
 
+window.addEventListener('keydown', keydown)
+
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+}
+
+async function keydown(e) {
+    // space is pressed
+    if (e.code == 'Space') {
+        // console.log(movement.state);
+        for (let i=0; i < 60; i++) {
+            if (movement.state == Direction.Forward) {
+                movement.rightward += 1 / 60;
+                movement.forward -= 1 / 60;
+                movement.rotationDelta += (1 / 60) * Math.PI / 2;
+            } else if (movement.state == Direction.Right) {
+                movement.rightward -= 1 / 60;
+                movement.forward += 1 / 60;
+                movement.rotationDelta -= (1 / 60) * Math.PI / 2;
+            }
+            await sleep(1000 / 60);
+        }
+
+        if (movement.state == Direction.Forward) movement.state = Direction.Right;
+        else if (movement.state == Direction.Right) movement.state = Direction.Forward;
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
